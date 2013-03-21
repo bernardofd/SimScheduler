9ed8e3f367aec7ed14e7c17e20656108fca2aa80
Bernardo Figuerêdo Domingues <bernardofd@gmail.com>
2013-03-21 20:48:13 -0300

    Avoid 'NoSuchElementException'

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -7,6 +7,9 @@ public class Scheduler {
 	};
 
 	public Process nextProcess(LinkedList<Process> readyQueue) {
+		if (readyQueue.size() <= 0) {
+			return null;
+		}
 		// Implementar aqui o algoritmo de escalonamento
 		// FCFS
 		return readyQueue.removeFirst();
